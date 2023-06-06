2827cd4892e5ceb4f8405195ed6c39117aef79be
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-06-06 17:35:00 +0000

    strings updated

--- strings.rb
+++ strings.rb
@@ -1,23 +1,23 @@
 # frozen_string_literal: true
 
-# interpolation
-# puts "2 + 2 = #{2 + 2}"
-
 message = 'Hello World'
+name = 'Alexander'
+
+# interpolation
+puts "2 + 2 = #{2 + 2}"
+puts "Hello, #{name}"
 
 # make uppercase
-# puts message.upcase
+puts message.upcase
 
 # reverse string
-# puts message.reverse
+puts message.reverse
 
-name = 'Alexander'
 # concatenation
-# puts message.sub(" World", ", ") + "Alexander"
-# puts "Hello, #{name}"
+puts message.sub(' World', ', ') + 'Alexander'
 
 # print multiple times
-# puts "#{message}\n" * 5
+puts "#{message}\n" * 5
 
 # Querying methods
 puts message.ascii_only?
@@ -26,3 +26,12 @@ puts name.include?('Alex')
 puts message.match?('Hello')
 puts name.start_with?('Hello')
 puts message.end_with?('Hello')
+
+# Method chaining
+puts message.downcase.include?('h')
+
+# Check if a string contains a substring(can take regex or string)
+puts message =~ /He/
+
+# using [-1] on any string automatically gives you the last letter of the string
+puts name[-1]
